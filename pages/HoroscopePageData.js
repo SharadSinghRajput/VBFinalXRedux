f8bf9029648d85cc9707f6a1fd0ab4df6c377542
Rohit Kumar <rohit.kumar@brandshow.in>
2024-06-04 16:13:54 +0530

    Added Right Click to open url in new tab

--- pages/HoroscopePageData.js
+++ pages/HoroscopePageData.js
@@ -157,10 +157,14 @@ export default function HoroscopePageData({data}) {
                     <h2 className="text-xl text-white font-bold text-center mb-4">Free Daily / Weekly / Monthly Horoscope</h2>
                     <div className="flex flex-row flex-wrap gap-3 justify-center ">
                       {Horoscope.map((person) => (
-                        <button key={person.name} onClick={()=> router.push(person.Link)} >
+                        <a  
+                            key={person.name} 
+                            href={`${MAIN_URL}${person.Link}`}
+                            onClick={(e) => handleClick(e, person.Link)}
+                        >
                             <Image width={50} height={50} className="bg-white h-10 w-10 bg-white w-[40px] h-[40px] md:w-[50px] md:h-[50px] lg:w-[75px] lg:h-[75px] rounded-[50px] flex flex-col justify-center items-center px-2 py-2 " src={person.imgSrc} alt="" />
                             <h3 className="mt-2 text-xs text-white text-base text-center font-normal leading-7 tracking-tight text-gray-900 leading-3">{person.name}</h3>
-                        </button>
+                        </a>
                       ))}
                     </div>
                   </div>
