58f99e79aa53000162bf26c91deafaadd50bae97
Rohit Kumar <rohit.kumar@brandshow.in>
2024-06-06 18:09:53 +0530

    Festival Componet Final

--- pages/AskQuestionList.js
+++ pages/AskQuestionList.js
@@ -1,90 +1,194 @@
-"use client"; 
-import { CheckIcon } from '@heroicons/react/20/solid'
 
-const tiers = [
-  {
-    name: 'Hobby',
-    id: 'tier-hobby',
-    href: '#',
-    priceMonthly: '$49',
-    description: 'Modi dolorem expedita deleniti. Corporis iste qui inventore pariatur adipisci vitae.',
-    features: ['5 products', 'Up to 1,000 subscribers', 'Basic analytics', '48-hour support response time'],
-  },
-  {
-    name: 'Team',
-    id: 'tier-team',
-    href: '#',
-    priceMonthly: '$79',
-    description: 'Explicabo quo fugit vel facere ullam corrupti non dolores. Expedita eius sit sequi.',
-    features: [
-      'Unlimited products',
-      'Unlimited subscribers',
-      'Advanced analytics',
-      '1-hour, dedicated support response time',
-      'Marketing automations',
-    ],
-  },
-]
+import React, { useEffect, useState, Fragment } from 'react';
 
-export default function Example({data}) {
+import 'react-datepicker/dist/react-datepicker.css';
+import 'swiper/css';
+import { useRouter } from 'next/router';
+import HoroscopeFetchAPI from '../config/horoscopeFetchAPI';
+import { setLocalStorageItem, getLocalStorageItem } from '../config/localStorage';
+import fetchAstrologyData from '../config/getAstroAPI';
+
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(' ')
+}
+
+
+  
+export default function Kundli({data}) {
+    const router = useRouter();
+    const [activeTab, setActiveTab] = useState(2);
+    const pageLanguage = data ? data.language || false : false;
+
+    const clients = [
+        {
+          id: 1,
+          name: 'Tuple',
+          imageUrl: 'https://tailwindui.com/img/logos/48x48/tuple.svg',
+          lastInvoice: { date: 'December 13, 2022', dateTime: '2022-12-13', amount: '2,000.00', currency: "$", status: 'Paid' },
+        },
+        {
+          id: 2,
+          name: 'SavvyCal',
+          imageUrl: 'https://tailwindui.com/img/logos/48x48/savvycal.svg',
+          lastInvoice: { date: 'January 22, 2023', dateTime: '2023-01-22', amount: '14,000.00', currency: "$", status: 'Paid' },
+        },
+        {
+          id: 3,
+          name: 'Reform',
+          imageUrl: 'https://tailwindui.com/img/logos/48x48/reform.svg',
+          lastInvoice: { date: 'January 23, 2023', dateTime: '2023-01-23', amount: '7,600.00', currency: "$", status: 'Paid' },
+        },
+      ]
+      const statuses = {
+        Paid: 'text-green-700 bg-green-50 ring-green-600/20',
+        Withdraw: 'text-gray-600 bg-gray-50 ring-gray-500/10',
+        Overdue: 'text-red-700 bg-red-50 ring-red-600/10',
+      }
+
+    const handleTabClick = (tabIndex) => {
+        setActiveTab(tabIndex);
+    };
+  
+
+    const [ZodiacSign, setZodiacSign] = useState("");
+    const [currentDate, setCurrentDate] = useState("");
+    const [currentDay, setcurrentDay] = useState("today");
+    const [Forecast, setForecast] = useState("");
+
+    useEffect(() => {
+        const GetDataThird = getLocalStorageItem('AstroDetailKey');
+        const AstroDet = getLocalStorageItem('AstroAPIHitDataKey');
+        if(GetDataThird && AstroDet){
+            fetchData(AstroDet)
+        }
+    }, []); 
+
+    const fetchData = async (AstroDet) => {
+        
+        const data = {
+            day: AstroDet.dobData.day,
+            month: AstroDet.dobData.month,
+            year: AstroDet.dobData.year,
+            hour: AstroDet.dobData.hour,
+            min: AstroDet.dobData.min,
+            lat: AstroDet.birth_place_latitude,
+            lon: AstroDet.birth_place_longitude,
+            tzone: AstroDet.tzone,
+        };
+        try {
+            const VimshottariDashaData = await fetchAstrologyData(data, `biorhythm`);
+            console.log("VimshottariDashaData",  VimshottariDashaData);
+        } catch (error) {
+        }
+    }
+    // const fetchData = async (sign, tZone) => {
+    //     const ZodiacSign = sign.toLowerCase()
+    //     const data = {
+    //         timezone: tZone
+    //     }
+    //     try {
+    //         const VimshottariDashaData = await fetchAstrologyData(data, `sun_sign_prediction/daily/${ZodiacSign}`);
+    //         setForecast(VimshottariDashaData);
+    //     } catch (error) {
+    //     }
+    // }
+
+
+
+    // useEffect(() => {
+    //     console.log("astrologyData");
+    //     const GetData = getLocalStorageItem('AstroAPIHitDataKey');
+    //     const GetDataSec = getLocalStorageItem('KundliFromDataKey');
+    //     const GetDataThird = getLocalStorageItem('AstroDetailKey');
+
+    //     if(GetData || GetDataSec || GetDataThird){
+    //         console.log("astrologyData 2");
+    //         const CapitalizedZodiac = GetDataThird.sign
+    //         const currentDate = GetDataSec.cdate
+
+    //         setZodiacSign(CapitalizedZodiac)
+    //         setCurrentDate(currentDate)
+
+    //         fetchCategoryWiseData(CapitalizedZodiac, currentDate)
+    //     }
+
+    // }, []); 
+
+    // const fetchCategoryWiseData = async (Zodiac, currentDate) => {
+    //     console.log("astrologyData 3");
+    //     const Sign = Zodiac || ZodiacSign
+    //     const date = currentDate || currentDate
+    //     const type = false
+
+    //     let [datePart, timePart] = date.split(' ');
+    //     let [month, day, year] = datePart.split('-');
+    //     let formattedDate = `${year}-${month}-${day}`;
+    //     console.log(formattedDate);
+    //     try {
+    //         const astrologyData = await HoroscopeFetchAPI(Sign, type, currentDay, formattedDate, pageLanguage);
+    //         console.log(astrologyData);
+            
+            
+    //       } catch (error) {
+    //         return null;
+    //       }
+    // };
+
   return (
-    <div className="isolate overflow-hidden bg-gray-900">
-      <div className="mx-auto max-w-7xl px-6 pb-96 pt-24 text-center sm:pt-32 lg:px-8">
-        <div className="mx-auto max-w-4xl">
-          
-          {data.title ?
-            <h1 className="mt-2 text-4xl font-bold tracking-tight text-white sm:text-5xl">
-                {data.title}
-            </h1>
-        :<></>}
-        </div>
-        <div className="relative mt-6">
-          <svg
-            viewBox="0 0 1208 1024"
-            className="absolute -top-10 left-1/2 -z-10 h-[64rem] -translate-x-1/2 [mask-image:radial-gradient(closest-side,white,transparent)] sm:-top-12 md:-top-20 lg:-top-12 xl:top-0"
-          >
-            <ellipse cx={604} cy={512} fill="url(#6d1bd035-0dd1-437e-93fa-59d316231eb0)" rx={604} ry={512} />
-            <defs>
-              <radialGradient id="6d1bd035-0dd1-437e-93fa-59d316231eb0">
-                <stop stopColor="#7775D6" />
-                <stop offset={1} stopColor="#E935C1" />
-              </radialGradient>
-            </defs>
-          </svg>
-        </div>
-      </div>
-      <div className="flow-root bg-orange-50 pb-24 sm:pb-32">
-        <div className="-mt-40">
-          <div className="mx-auto max-w-7xl px-6 lg:px-8">
-            <div className="mx-auto grid max-w-md grid-cols-1 gap-8 lg:max-w-4xl lg:grid-cols-2">
-              {tiers.map((tier) => (
-                <div
-                  key={tier.id}
-                  className="flex flex-col justify-between rounded-3xl bg-white p-8 shadow-xl ring-1 ring-gray-900/10 sm:p-10"
-                >
-                  <div>
-                    <h3 id={tier.id} className="text-base font-semibold leading-7 text-indigo-600">
-                      {tier.name}
-                    </h3>
-                    <div className="mt-4 flex items-baseline gap-x-2">
-                      <span className="text-5xl font-bold tracking-tight text-gray-900">{tier.priceMonthly}</span>
-                    </div>
-                    <p className="mt-6 text-base leading-7 text-gray-600">{tier.description}</p>
-                  </div>
-                  <a
-                    href={tier.href}
-                    aria-describedby={tier.id}
-                    className="mt-8 block rounded-md bg-orange-600 px-3.5 py-2 text-center text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                  >
-                    Click To Ask Question
-                  </a>
+    <>
+    {data ? (
+    <div className="pt-5">
+        <div className={`bg-white mx-auto max-w-6xl mx-auto shadow-2xl p-5 pt-5 mb-5 rounded-lg`}>
+            {data.title ? <>
+                <h1 className='text-lg font-bold mb-5'>
+                    {data.title}
+                </h1>
+            </>:<></>}
+            <div className="flex flex-col">
+                <div className="mx-auto max-w-2xl lg:mx-0 lg:max-w-none">
+                    <ul role="list" className="mt-3 grid grid-cols-1 gap-x-6 gap-y-8 lg:grid-cols-3 xl:gap-x-8">
+                        {clients.map((client) => (
+                        <li key={client.id} className="overflow-hidden rounded-xl border border-gray-200">
+                            <div className="flex items-center gap-x-4 border-b border-gray-900/5 bg-gray-50 p-3">
+                            <img
+                                src={client.imageUrl}
+                                alt={client.name}
+                                className="h-12 w-12 flex-none rounded-lg bg-white object-cover ring-1 ring-gray-900/10"
+                            />
+                            <div className="text-sm font-medium leading-6 text-gray-900">{client.name}</div>
+                            
+                            </div>
+                            <dl className="-my-3 divide-y divide-gray-100 px-6 py-4 text-sm leading-6">
+                            <div className="flex justify-between gap-x-4 py-3">
+                                <dt className="text-gray-500">Price: </dt>
+                                <dd className="flex items-start gap-x-2">
+                                <div
+                                    className={classNames(
+                                    statuses[client.lastInvoice.status],
+                                    'rounded-md py-1 px-2 text-xs font-medium ring-1 ring-inset'
+                                    )}
+                                >{client.lastInvoice.currency} {client.lastInvoice.amount}
+                                </div>
+                                </dd>
+                            </div>
+                            <a
+                                type="button"
+                                className="inline-block items-center cursor-pointer text-center gap-x-1.5 rounded-md bg-orange-600 px-2.5 py-1.5 text-sm font-semibold text-white shadow-sm hover:bg-orange-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-orange-600 mb-5 w-full mt-2"
+                            >
+                                Click to Ask Question
+                            </a>
+                            </dl>
+                        </li>
+                        ))}
+                    </ul>
                 </div>
-              ))}
-              
             </div>
-          </div>
         </div>
-      </div>
     </div>
+    ) : (
+        <></>
+    )}
+    </>
   )
 }
