bd27d606581a708250a01ea3ee707f412d77cfe5
Rohit Kumar <rohit.kumar@brandshow.in>
2024-06-10 16:37:20 +0530

    Getting Updated Files

--- config/config.js
+++ config/config.js
@@ -1,8 +1,8 @@
 export const API_NEW_URL = "https://www.aapkikismat.com/";
-export const MAIN_URL = "https://dolphin-app-8x2ht.ondigitalocean.app/";
-// export const MAIN_URL = "http://localhost:3000/";
+// export const MAIN_URL = "https://dolphin-app-8x2ht.ondigitalocean.app/";
+export const MAIN_URL = "http://localhost:3000/";
 // export const MAIN_URL_HINDI = "https://dolphin-app-8x2ht.ondigitalocean.app/hindi/";
-export const MAIN_URL_HINDI = "https://dolphin-app-8x2ht.ondigitalocean.app/hindi/";
+export const MAIN_URL_HINDI = "http://localhost:3000/hindi/";
 
 export const API_KEY = "Sd_12547455dhseunhT_ksdfln";
 export const Domain_Secrete_Code = "8f9da951515975a3fbccc9fd582eb7bf";
