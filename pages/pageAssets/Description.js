c37f46f59df45774deb211de1e7188031af604ea
Rohit Kumar <rohit.kumar@brandshow.in>
2024-06-11 10:33:24 +0530

    Getting Updated Files 11th Jun

--- pages/pageAssets/Description.js
+++ pages/pageAssets/Description.js
@@ -3,6 +3,7 @@ export default function Banner(props) {
   const descHTML = props.descData ? props.descData.replace(/\\r\\n/g, '<br/>') : '';
 
   return (
-    <div className={`mb-2 ml-2 mr-2 text-black inner-data-mb-5 text-justify ${props.extra ? props.extra : "text-sm"}`} dangerouslySetInnerHTML={{ __html: descHTML }} />
+    <div className={`mb-2 ml-2 mr-2 text-black inner-data-mb-5 text-justify ${props.extra ? props.extra : "text-sm "} has-[ol]:!list-decimal has-[ul]:!list-disc has-[li]:!ml-2`} dangerouslySetInnerHTML={{ __html: descHTML }} />
+
   );
 }
