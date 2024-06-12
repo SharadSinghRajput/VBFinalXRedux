f30c2f1f731d881e687bbf14a61fcd8b26cbc3b0
Ashish Verma <ashishverma@Ashishs-Mac-mini.local>
2024-06-12 13:08:52 +0530

    link change

--- pages/Banner2.js
+++ pages/Banner2.js
@@ -8,9 +8,9 @@ export default function Questions({language = "English"}) {
     const router = useRouter();
     const [mainURL, setMainURL] = useState(MAIN_URL);
     const BannerImages = [
-        { urlHindi: "https://www.vinaybajrangi.com/upload/manage-image-banner/259hin_Know-How-Your-Future-Life-Partne%20(1).jpg", url: "https://www.vinaybajrangi.com/upload/manage-image-banner/259_Know What Your Future Life Partner Will Be Like.jpg", alt: "Vinay Bajrangi", link: "services/online-report/future-life-partner.php" },
-        { urlHindi: "https://www.vinaybajrangi.com/upload/manage-image-banner/260hin_Know-When-You-Will-Get-Married-3%20(1).jpg", url: "https://www.vinaybajrangi.com/upload/manage-image-banner/260_Know When You Will Get Married (2).jpg", alt: "Vinay Bajrangi", link: "services/online-report/know-your-marriage-timing.php" },
-        { urlHindi: "https://www.vinaybajrangi.com/upload/manage-image-banner/261hin_Know-Do-You-have-the-Yoga-to-Do-%20(1).jpg", url: "https://www.vinaybajrangi.com/upload/manage-image-banner/261_Know If You Have The Yoga to Do Business.jpg", alt: "Vinay Bajrangi", link: "services/online-report/job-or-business.php" }
+        { urlHindi: "https://www.vinaybajrangi.com/upload/manage-image-banner/259hin_Know-How-Your-Future-Life-Partne%20(1).jpg", url: "https://www.vinaybajrangi.com/upload/manage-image-banner/259_Know What Your Future Life Partner Will Be Like.jpg", alt: "Vinay Bajrangi", link: "/services/online-reports/future-life-partner.php" },
+        { urlHindi: "https://www.vinaybajrangi.com/upload/manage-image-banner/260hin_Know-When-You-Will-Get-Married-3%20(1).jpg", url: "https://www.vinaybajrangi.com/upload/manage-image-banner/260_Know When You Will Get Married (2).jpg", alt: "Vinay Bajrangi", link: "/services/online-reports/know-your-marriage-timing.php" },
+        { urlHindi: "https://www.vinaybajrangi.com/upload/manage-image-banner/261hin_Know-Do-You-have-the-Yoga-to-Do-%20(1).jpg", url: "https://www.vinaybajrangi.com/upload/manage-image-banner/261_Know If You Have The Yoga to Do Business.jpg", alt: "Vinay Bajrangi", link: "/services/online-reports/job-or-business.php" }
     ]
 
     useEffect(() => {
