b2a34791be96b08e6ed0369e6d26a5b4806edf34
Ashish Verma <ashishverma@Ashishs-Mac-mini.local>
2024-06-15 13:15:47 +0530

    change in hindi menu

--- pages/pageAssets/KundliForm.js
+++ pages/pageAssets/KundliForm.js
@@ -111,10 +111,20 @@ const handleClick = (e, url) => {
     //     return result;
     //   }
         const convertDateTime = (dateTimeStr) => {
+            // const date = new Date(dateTimeStr);
+            // const updatedDate = date.toISOString().split('T')[0];
+            // const timeParts = date.toISOString().split('T')[1].split('.')[0];
+            // return `${updatedDate} ${timeParts}`;
             const date = new Date(dateTimeStr);
-            const updatedDate = date.toISOString().split('T')[0];
-            const timeParts = date.toISOString().split('T')[1].split('.')[0];
-            return `${updatedDate} ${timeParts}`;
+            const year = date.getFullYear();
+            const month = String(date.getMonth() + 1).padStart(2, '0');
+            const day = String(date.getDate()).padStart(2, '0');
+            const hours = String(date.getHours()).padStart(2, '0');
+            const minutes = String(date.getMinutes()).padStart(2, '0');
+            const seconds = String(date.getSeconds()).padStart(2, '0');
+          
+            const formattedDate = `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+            return formattedDate;
         };
 
         const formattedDateTime = convertDateTime(selectedDateTime.toISOString())
@@ -129,7 +139,6 @@ const handleClick = (e, url) => {
           }
           
           const CurrentDate = formatDateIn(new Date());
-          console.log(formattedDateTime);
 
           const [DatePart] = formattedDateTime.split(" ");
           const dataForTimeZone = {
