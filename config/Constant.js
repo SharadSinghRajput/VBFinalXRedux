5cc9485906daaa22e3b627ad33c9ce906a2115f3
Ashish Verma <ashishverma@Ashishs-Mac-mini.local>
2024-07-10 10:19:24 +0530

    new push

--- config/Constant.js
+++ config/Constant.js
@@ -47,7 +47,7 @@ export const MainMenu = [
     },
     { name: 'Astrology', url : '#',
         sublinks: [
-            { name: 'Today Panchang', url : `${MAIN_URL}today-panchang.php`, },
+            { name: 'Today Panchang', url : `${MAIN_URL}panchang`, },
             { name: 'Festivals', url : `${MAIN_URL}festivals.php`, },
             { 
                 name: "Planetary Transit",
