ecd9da7ff7d4188d2b71bb57e0ff6f655faeb1b7
Ashish Verma <ashishverma@Ashishs-Mac-mini.local>
2024-07-12 18:45:10 +0530

    new

--- config/config.js
+++ config/config.js
@@ -4,4 +4,4 @@ export const MAIN_URL = process.env.NEXT_PUBLIC_MAIN_URL;
 export const MAIN_URL_HINDI = process.env.NEXT_PUBLIC_MAIN_URL_HINDI;
 
 export const API_KEY = "Sd_12547455dhseunhT_ksdfln";
-export const Domain_Secrete_Code = "8f9da951515975a3fbccc9fd582eb7bf";
+export const Domain_Secrete_Code = "";
