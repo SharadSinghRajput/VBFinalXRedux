4245dd93d9ee368d1b77f05a13f5c1c23883ffdc
Ashish Verma <ashishverma@Ashishs-Mac-mini.local>
2024-07-12 19:09:00 +0530

    new push

--- config/ConsultationData.js
+++ config/ConsultationData.js
@@ -14,57 +14,57 @@ const Consultation = [
         links: [
             {
                 text: "consultation for DELAY IN...",
-                url: "marriage-astrology/late-marriage",
+                url: "/marriage-astrology/late-marriage",
                 imgsrc: Img61,
                 alt: "Consultation for Delay in Marriage",
                 price: "7100.00"
             },
             {
                 text: "Consultation for Divorce",
-                url: "services/consultation/divorce.php",
+                url: "/services/consultation/divorce.php",
                 imgsrc: Img82,
                 alt: "Consultation for Divorce",
                 price: "7100.00"
             },
             {
                 text: "Consultation for Future Life...",
-                url: "services/consultation/life-partner.php",
+                url: "/services/consultation/life-partner.php",
                 imgsrc: Img88,
                 price: "7100.00"
             },
             {
                 text: "Consultation for Horoscope...",
-                url: "services/consultation/horoscope-matching.php",
+                url: "/services/consultation/horoscope-matching.php",
                 imgsrc: Img89,
                 price: "7100.00"
             },
             {
                 text: "Consultation for Love Marriage...",
-                url: "services/consultation/love-marriage-issues.php",
+                url: "/services/consultation/love-marriage-issues.php",
                 imgsrc: Img60,
                 price: "7100.00"
             },
             {
                 text: "Consultation for Manglik Dosh...",
-                url: "services/consultation/manglik-dosh.php",
+                url: "/services/consultation/manglik-dosh.php",
                 imgsrc: Img83,
                 price: "7100.00"
             },
             {
                 text: "Consultation for Married Life...",
-                url: "services/consultation/married-life-issues.php",
+                url: "/services/consultation/married-life-issues.php",
                 imgsrc: Img62,
                 price: "7100.00"
             },
             {
                 text: "Consultation for Post marriage...",
-                url: "services/consultation/post-marriage-counseling.php",
+                url: "/services/consultation/post-marriage-counseling.php",
                 imgsrc: Img285,
                 price: "31000.00"
             },
             {
                 text: "Consultation for Pre-Marriage...",
-                url: "services/consultation/pre-marriage-counseling.php",
+                url: "/services/consultation/pre-marriage-counseling.php",
                 imgsrc: Img287,
                 price: "20000.00"
             },
--- config/json/lifesProblems.js
+++ config/json/lifesProblems.js
@@ -16,20 +16,20 @@ import vfc from "../../pages/assets/images/Solutions/vfc.png"
 
 
 export const lifesProblems = [
-  { name: "Loan and Debt", img: loanDebt, url: "/loan-and-debts.php", hindiName: "ऋण और कर्ज़", hindiUrl: "finance-astrology-in-hindi" },
-  { name: "Children Astrology", img: childrenAstrology, url: "/children-astrology.php", hindiName: "बाल ज्योतिष", hindiUrl: "children-astrology-in-hindi" },
-  { name: "Betting & Gambling", img: Icons, url: "/astrology-for-betting.php", hindiName: "सट्टेबाजी और जुआ", hindiUrl: "astrology-for-betting-in-hindi" },
-  { name: "All about Vastu", img: allaboutVastu, url: "/vastu.php", hindiName: "वास्तु ज्योतिष", hindiUrl: "vastu-in-hindi" },
-  { name: "Foreign Settlement", img: foreignSettlement, url: "/foreign-travel.php", hindiName: "कुंडली में विदेश में बसाव", hindiUrl: "foreign-settlement-astrology-in-hindi" },
-  { name: "Vastu for commercial", img: vfc, url: "/vastu-for-commercial.php", hindiName: "व्यावसायिक वास्तु", hindiUrl: "vastu-for-commercial-in-hindi" },
-  { name: "Past life Readings", img: pastlifeReadings, url: "/life-predictions.php", hindiName: "पिछले जीवन का विश्लेषण", hindiUrl: "past-life-reading-in-hindi" },
-  { name: "Share market Astrology", img: birthTimeCorrection, url: "/share-market-astrology.php", hindiName: "शेयर बाजार और शेयर व्यापार ज्योतिष", hindiUrl: "share-market-astrology-in-hindi" },
-  { name: "Birth time Correction", img: btc, url: "/time-rectification.php", hindiName: " जन्म समय सुधार", hindiUrl: "hindi/know-birth-time.php" },
-  { name: "Marriage Astrology", img: fileIconsRing, url: "/marriage-astrology.php", hindiName: "विवाह ज्योतिष", hindiUrl: "marriage-astrology-in-hindi" },
-  { name: "Property Astrology", img: propertyAstrology, url: "/property.php", hindiName: "संपत्ति ज्योतिष", hindiUrl: "property-astrology-in-hindi" },
-  { name: "Business Astrology", img: ba, url: "/business-astrology.php", hindiName: "बिजनेस ज्योतिष", hindiUrl: "business-astrology-in-hindi" },
+  { name: "Loan and Debt", img: loanDebt, url: "/loan-and-debts.php", hindiName: "ऋण और कर्ज़", hindiUrl: "/finance-astrology-in-hindi" },
+  { name: "Children Astrology", img: childrenAstrology, url: "/children-astrology.php", hindiName: "बाल ज्योतिष", hindiUrl: "/children-astrology-in-hindi" },
+  { name: "Betting & Gambling", img: Icons, url: "/astrology-for-betting.php", hindiName: "सट्टेबाजी और जुआ", hindiUrl: "/astrology-for-betting-in-hindi" },
+  { name: "All about Vastu", img: allaboutVastu, url: "/vastu.php", hindiName: "वास्तु ज्योतिष", hindiUrl: "/vastu-in-hindi" },
+  { name: "Foreign Settlement", img: foreignSettlement, url: "/foreign-travel.php", hindiName: "कुंडली में विदेश में बसाव", hindiUrl: "/foreign-settlement-astrology-in-hindi" },
+  { name: "Vastu for commercial", img: vfc, url: "/vastu-for-commercial.php", hindiName: "व्यावसायिक वास्तु", hindiUrl: "/vastu-for-commercial-in-hindi" },
+  { name: "Past life Readings", img: pastlifeReadings, url: "/life-predictions.php", hindiName: "पिछले जीवन का विश्लेषण", hindiUrl: "/past-life-reading-in-hindi" },
+  { name: "Share market Astrology", img: birthTimeCorrection, url: "/share-market-astrology.php", hindiName: "शेयर बाजार और शेयर व्यापार ज्योतिष", hindiUrl: "/share-market-astrology-in-hindi" },
+  { name: "Birth time Correction", img: btc, url: "/time-rectification.php", hindiName: " जन्म समय सुधार", hindiUrl: "/hindi/know-birth-time.php" },
+  { name: "Marriage Astrology", img: fileIconsRing, url: "/marriage-astrology.php", hindiName: "विवाह ज्योतिष", hindiUrl: "/marriage-astrology-in-hindi" },
+  { name: "Property Astrology", img: propertyAstrology, url: "/property.php", hindiName: "संपत्ति ज्योतिष", hindiUrl: "/property-astrology-in-hindi" },
+  { name: "Business Astrology", img: ba, url: "/business-astrology.php", hindiName: "बिजनेस ज्योतिष", hindiUrl: "/business-astrology-in-hindi" },
   { name: "Court / legal Issues", img: courtLegalIssues, url: "/court-cases.php", hindiName: "कोर्ट केस/कानूनी मुकदमे", hindiUrl: "/hindi/court-case-astrology.php" },
-  { name: "Career Astrology", img: careerAstrology, url: "/career-astrology.php", hindiName: "करियर ज्योतिष", hindiUrl: "career-astrology-in-hindi" },
-  { name: "Health Astrology", img: Ha, url: "/health-astrology.php", hindiName: "स्वास्थ्य ज्योतिष", hindiUrl: "health-astrology-in-hindi" }
+  { name: "Career Astrology", img: careerAstrology, url: "/career-astrology.php", hindiName: "करियर ज्योतिष", hindiUrl: "/career-astrology-in-hindi" },
+  { name: "Health Astrology", img: Ha, url: "/health-astrology.php", hindiName: "स्वास्थ्य ज्योतिष", hindiUrl: "/health-astrology-in-hindi" }
 
 ]
